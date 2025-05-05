a4fd1fbf4a1c0b1ced5fe1094490391b2b1cae1e
Carol Torres <acstorrea49@gmail.com>
2025-05-05 18:38:46 -0300

    meu primeiro commit

---
+++ ex 2.py
@@ -0,0 +1,1 @@
+print('Qual seu nome')
---
+++ python exercice.py
@@ -0,0 +1,8 @@
+lista = [101, 102, 103, 104, 105, 106]
+print(f' lista[0] = {[lista[0]]}')
+print(f' lista[1] = {[lista[1]]}')
+print(f' lista[2] = {[lista[2]]}')
+print(f' lista[3] = {[lista[3]]}')
+print(f' lista[4] = {[lista[4]]}')
+print(f' lista[5] = {[lista[5]]}')
+print(f' a lista completa é: {lista}')
