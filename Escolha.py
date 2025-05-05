9891b13b32ad13678176ce83f1e6f6522c2a3a54
Carol Torres <acstorrea49@gmail.com>
2025-05-05 18:44:09 -0300

    Escolha

--- Escolha.py
+++ Escolha.py
@@ -4,5 +4,6 @@ n2 = str(input('Segundo aluno(a): '))
 n3 = str(input('Terceiro aluno (a): '))
 n4 = str(input('Quarto aluno: (a) '))
 lista = [n1, n2, n3, n4]
-escolhido = choice(lista)
+escolhido = (
+    choice(lista))
 print(f'O escolhido(a) foi: {escolhido}')
